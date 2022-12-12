d99d1fc8c06bf4a6bdfb1413f13c5c863447b4f8
akirahani <kuroshinichi2109@gmail.com>
2022-12-12 11:59:52 +0700

    ok

--- app/src/main/java/com/example/nhatro2/dich_vu/DichVu.java
+++ app/src/main/java/com/example/nhatro2/dich_vu/DichVu.java
@@ -1,16 +1,70 @@
 package com.example.nhatro2.dich_vu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.nhatro2.MainActivity;
 import com.example.nhatro2.R;
 
-public class DichVu extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class DichVu extends AppCompatActivity {
+    ImageView thoat;
+    SharedPreferences shp;
+    RecyclerView listThietBi;
+    TextView tenThietBi, giaThietBi, tacVu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dich_vu);
+        // Nút thoát
+        thoat = findViewById(R.id.thoat);
+        thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(DichVu.this);
+                builder.setTitle("Confirm").setMessage("Bạn có thực sự muốn thoát ?");
+                builder.setCancelable(true);
+                builder.setIcon(R.drawable.alert_bottom);
+                //check
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(DichVu.this,"Out", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(DichVu.this, MainActivity.class);
+                        startActivity(intent);
+                        shp = view.getContext().getSharedPreferences("user", MODE_PRIVATE);
+                        shp.edit().clear().commit();
+//                        view.getContext();
+                    }
+                });
+                // NO
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(DichVu.this,"Stay", Toast.LENGTH_SHORT).show();
+                        //  Cancel
+                        dialog.cancel();
+                    }
+                });
+                // show alert
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+        // Thiết bị
+        listThietBi.setLayoutManager(new GridLayoutManager(DichVu.this, 3));
+        listThietBi.hasFixedSize();
+        listThietBi.setNestedScrollingEnabled(false);
     }
 }
