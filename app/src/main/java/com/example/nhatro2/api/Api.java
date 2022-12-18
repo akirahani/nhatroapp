331a3a615508981c9622a169ee5419d3a4ca76f9
pon <kuroshinichi2109@gmail.com>
2022-12-18 21:05:50 +0700

    ok

--- app/src/main/java/com/example/nhatro2/api/Api.java
+++ app/src/main/java/com/example/nhatro2/api/Api.java
@@ -18,8 +18,8 @@ import retrofit2.http.POST;
 
 
 public interface Api {
-    String url = "http://172.16.1.71";
-//    String url = "http://192.168.0.104";
+//    String url = "http://172.16.1.71";
+    String url = "http://192.168.0.104";
     //Init
     Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-DD HH:mm:ss").create();
     OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
@@ -30,49 +30,49 @@ public interface Api {
             .build()
             .create(Api.class);
 
-    @POST ("/nhatro/admin/api/auth/login.php")
+    @POST ("/nhatro2/admin/api/auth/login.php")
     @FormUrlEncoded
     Call <ThanhVienModel> postLogin(@Field("username") String username,
                                     @Field("password") String password);
 
     // Phòng trống
-    @GET ("/nhatro/admin/api/phong/list.php")
+    @GET ("/nhatro2/admin/api/phong/list.php")
     Call<List<PhongModel>> getPhongList();
     // Phòng đặt
-    @GET ("/nhatro/admin/api/phong/book.php")
+    @GET ("/nhatro2/admin/api/phong/book.php")
     Call<List<PhongModel>> getBookRoomList();
     // Phòng thuê
-    @GET ("/nhatro/admin/api/phong/rent.php")
+    @GET ("/nhatro2/admin/api/phong/rent.php")
     Call<List<PhongModel>> getRentRoomList();
     // Khách thuê
-    @GET ("/nhatro/admin/api/khach/list.php")
+    @GET ("/nhatro2/admin/api/khach/list.php")
 
     Call<List<ThanhVienModel>> getKhachList();
 
-    @POST ("/nhatro/admin/api/phong/add.php")
+    @POST ("/nhatro2/admin/api/phong/add.php")
     @FormUrlEncoded
     Call <POST> addPhong(@Field("tenphong") String tenphong,
                          @Field("trangthai") int trangthai,
                          @Field("vitri") int vitri);
     //Dịch vụ
-    @GET ("/nhatro/admin/api/dichvu/list.php")
+    @GET ("/nhatro2/admin/api/dichvu/list.php")
     Call<List<DichVuModel>> getDichVuList();
     //Thêm Dịch vụ
-    @POST("/nhatro/admin/api/dichvu/add.php")
+    @POST("/nhatro2/admin/api/dichvu/add.php")
     @FormUrlEncoded
     Call<DichVuModel> addThietBi(@Field("ten") String tenthietbi, @Field("gia") Integer giathietbi);
     // Get Thông tin dịch vụ
-    @POST ("/nhatro/admin/api/dichvu/detail.php")
+    @POST ("/nhatro2/admin/api/dichvu/detail.php")
     @FormUrlEncoded
     Call <DichVuModel> detailDichVu (@Field("id") int id);
     // Cập nhật Thông tin dịch vụ
-    @POST ("/nhatro/admin/api/dichvu/edit.php")
+    @POST ("/nhatro2/admin/api/dichvu/edit.php")
     @FormUrlEncoded
     Call <DichVuModel> editDichVu (@Field("id") int id,
                                    @Field("ten") String ten,
                                    @Field("gia") int gia);
     // Xóa dịch vụ
-    @POST ("/nhatro/admin/api/dichvu/del.php")
+    @POST ("/nhatro2/admin/api/dichvu/del.php")
     @FormUrlEncoded
     Call <DichVuModel> delDichVu (@Field("id") int id);
 }
