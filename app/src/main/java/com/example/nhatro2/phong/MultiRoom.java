5ea853a09beb6df5a3af44a8ee4a7c6adff124d3
akirahani <kuroshinichi2109@gmail.com>
2022-12-22 17:47:58 +0700

    ok:

--- app/src/main/java/com/example/nhatro2/phong/MultiRoom.java
+++ app/src/main/java/com/example/nhatro2/phong/MultiRoom.java
@@ -2,10 +2,12 @@ package com.example.nhatro2.phong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.nhatro2.R;
 import com.example.nhatro2.api.Api;
@@ -25,7 +27,10 @@ import retrofit2.http.POST;
 public class MultiRoom extends AppCompatActivity {
 
     SharedPreferences sharedPhong;
-    String listRoom;
+    String listRoom, tenPhongchecked;
+    TextView nameRoomChecked;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,18 +39,23 @@ public class MultiRoom extends AppCompatActivity {
 //        listRoom = sharedPhong.getString("items", "");
         Bundle bundle = getIntent().getExtras();
         listRoom = bundle.getString("idRoom");
-        Log.d("tag",""+listRoom);
-//        Api.api.phongChecked(listRoom).enqueue(new Callback<PhongModel>() {
-//            @Override
-//            public void onResponse(Call<PhongModel> call, Response<PhongModel> response) {
-//                PhongModel phongCheck = response.body();
-//                Log.d("",""+phongCheck.getTen());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<PhongModel> call, Throwable t) {
-//
-//            }
-//        });
+        nameRoomChecked = findViewById(R.id.nameRoomChecked);
+        Api.api.phongChecked(listRoom).enqueue(new Callback<List<PhongModel>>() {
+            @Override
+            public void onResponse(Call<List<PhongModel>> call, Response<List<PhongModel>> response) {
+                List<PhongModel> phongCheck = response.body();
+                int sizePhong = phongCheck.size();
+                for (int i = 0; i < sizePhong; i++) {
+                    tenPhongchecked = phongCheck.get(i).getTen();
+                    nameRoomChecked.setText(tenPhongchecked);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<PhongModel>> call, Throwable t) {
+                Log.d("err", "" + t.toString());
+            }
+        });
     }
 }
