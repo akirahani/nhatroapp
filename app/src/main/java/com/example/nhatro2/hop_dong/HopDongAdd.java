f3e0955bea65f412af2b10b2848c226a11bfef69
akirahani <kuroshinichi2109@gmail.com>
2023-01-13 08:53:06 +0700

    ok

--- app/src/main/java/com/example/nhatro2/hop_dong/HopDongAdd.java
+++ app/src/main/java/com/example/nhatro2/hop_dong/HopDongAdd.java
@@ -143,7 +143,6 @@ public class HopDongAdd extends AppCompatActivity {
         String dienThoai = bundle.getString("dienthoai");
         int tienCoc = bundle.getInt("datcoc");
 
-
         listThietBi = findViewById(R.id.thietbiCheck);
         listThietBi.setLayoutManager(new GridLayoutManager(HopDongAdd.this, 3));
         listThietBi.hasFixedSize();
