864971f874a42b2b784e857b592e70d16116ac7d
akirahani <kuroshinichi2109@gmail.com>
2023-03-06 12:01:41 +0700

    ok

--- app/src/main/java/com/example/nhatro2/doi_thiet_bi/DoiThietBi.java
+++ app/src/main/java/com/example/nhatro2/doi_thiet_bi/DoiThietBi.java
@@ -1,6 +1,8 @@
 package com.example.nhatro2.doi_thiet_bi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -8,6 +10,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -16,14 +19,22 @@ import android.widget.LinearLayout;
 import com.example.nhatro2.HomeActivity;
 import com.example.nhatro2.R;
 import com.example.nhatro2.chi_khac.ChiKhac;
+import com.example.nhatro2.dong_tien.DongTien;
+import com.example.nhatro2.hop_dong.HopDong;
+import com.example.nhatro2.thanhvien.KhachTro;
+import com.example.nhatro2.thay_cong_to.CongToDien;
+import com.example.nhatro2.thay_cong_to.CongToNuoc;
+import com.example.nhatro2.tien_coc.TienCocAdd;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class DoiThietBi extends AppCompatActivity {
-    ImageView thoat, logo, themNguoiThue, menuDanhMuc;
+    ImageView thoat, logo, menuDanhMuc;
     SharedPreferences shp;
     LinearLayout quayLai;
     SharedPreferences.Editor shpKhachEdit;
     DrawerLayout mDrawerLayout;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,5 +65,53 @@ public class DoiThietBi extends AppCompatActivity {
         params.leftMargin = 46;
         params.topMargin = 18;
         imageChiKhac.addView(iv, params);
+
+        // Menu slide
+        menuDanhMuc = findViewById(R.id.menuDanhMuc);
+        mDrawerLayout = findViewById(R.id.drawer_layout_quan_li_thiet_bi);
+
+        menuDanhMuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.khach_tro:
+                        Intent khachTro = new Intent(DoiThietBi.this, KhachTro.class);
+                        startActivity(khachTro);
+                        return true;
+                    case R.id.dat_coc:
+                        Intent datCoc = new Intent(DoiThietBi.this, TienCocAdd.class);
+                        startActivity(datCoc);
+                        return true;
+                    case R.id.thanh_toan:
+                        Intent thanhToan = new Intent(DoiThietBi.this, DongTien.class);
+                        startActivity(thanhToan);
+                        return true;
+                    case R.id.hop_dong:
+                        Intent hopDong = new Intent(DoiThietBi.this, HopDong.class);
+                        startActivity(hopDong);
+                        return true;
+                    case R.id.thay_cong_to_nuoc:
+                        Intent thay_cong_to_nuoc = new Intent(DoiThietBi.this, CongToNuoc.class);
+                        startActivity(thay_cong_to_nuoc);
+                        return true;
+                    case R.id.thay_cong_to_dien:
+                        Intent thay_cong_to_dien = new Intent(DoiThietBi.this, CongToDien.class);
+                        startActivity(thay_cong_to_dien);
+                        return true;
+                    case R.id.doi_thiet_bi:
+                        Intent doi_thiet_bi = new Intent(DoiThietBi.this, DoiThietBi.class);
+                        startActivity(doi_thiet_bi);
+                        return true;
+                }
+                return true;
+            }
+        });
     }
 }
